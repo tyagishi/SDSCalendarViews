66695baeb3b43e081650f17652a9c379c81cda25
Tomoaki Yagishita <tyagishi@gmail.com>
2024-08-12 13:14:11 +0900

    update SwiftLint setting

--- Package.swift
+++ Package.swift
@@ -20,29 +20,18 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/tyagishi/SDSViewExtension", from: "4.0.0"),
         .package(url: "https://github.com/tyagishi/SDSSwiftExtension", from: "2.1.0"),
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.56.1"),
+
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SDSCalendarViews",
-            dependencies: ["SDSViewExtension", "SDSSwiftExtension", "SwiftLintXcode"]),
-        // 1. Specify where to download the compiled swiftlint tool from.
-        .binaryTarget(
-            name: "SwiftLintBinary",
-            url: "https://github.com/juozasvalancius/SwiftLint/releases/download/spm-accommodation/SwiftLintBinary-macos.artifactbundle.zip",
-            checksum: "cdc36c26225fba80efc3ac2e67c2e3c3f54937145869ea5dbcaa234e57fc3724"
-        ),
-        // 2. Define the SPM plugin.
-        .plugin(
-            name: "SwiftLintXcode",
-            capability: .buildTool(),
-            dependencies: ["SwiftLintBinary"]
-        ),
-        // 3. Use the plugin in your project.
-//        .executableTarget(
-//            name: "SDSCalendarViews",
-//            plugins: ["SwiftLintXcode"]
-//        )
+            dependencies: ["SDSViewExtension", "SDSSwiftExtension"],
+            plugins: [
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
+            ]
+        )
     ]
 )
